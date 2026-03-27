182eed808f6cd437853fbcb521a0b5768cd654ea
Connor <DeloreanCTM@Gmail.com>
2026-03-26 23:47:01 -0500

    First Commit, Basic Content

---
+++ main.js
@@ -0,0 +1,35 @@
+const goals = [];   
+
+function createGoal(goalName, goalContent){
+    return{
+        name: goalName, 
+        content: goalContent, 
+        completed: false
+    };
+}
+
+function changeGoal(goal, newContent){
+    goal.content = newContent;
+}
+
+function returnGoalName(goal){
+    return goal.name; 
+}
+
+function returnGoalContent(goal){
+    return goal.content; 
+}
+
+function changeGoalName(goal, newName){
+    goal.name = newName; 
+
+}
+
+
+function deleteGoal(name, goals[]){
+    for (let i=0; i < goal.length){
+        let temp = goals[i].
+    }
+
+
+}
---
+++ track.js
@@ -0,0 +1,10 @@
+function createLapTime(){
+
+
+}
+
+
+
+function changeLapYardage(){
+    
+}
