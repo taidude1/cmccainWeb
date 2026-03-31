85c330e14b3cf52ed587876821222f7e2e55709f
Connor <DeloreanCTM@Gmail.com>
2026-03-30 23:30:34 -0500

    New changes to the Main Js Functions, Goals

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const goals = [];   
 
+// function that creates the goal objects that we will be using 
 function createGoal(goalName, goalContent){
     return{
         name: goalName, 
@@ -7,6 +8,28 @@ function createGoal(goalName, goalContent){
         completed: false
     };
 }
+
+
+
+// functions that add the following goals or subtract them from the overall list 
+
+function addGoalToList(goal, goals){
+    goals.push(goal); 
+}
+
+function removeGoal(goals, goal){
+    for(let i = 0; i < goals.length; i++){
+        if (returnGoalName(goals[i]) == returnGoalName(goal)){
+        goals.splice(i, 1); 
+        break; 
+        }
+    }
+
+    return goals;
+}
+
+
+// functions that simply change the goals statements and return them 
 
 function changeGoal(goal, newContent){
     goal.content = newContent;
@@ -25,11 +48,22 @@ function changeGoalName(goal, newName){
 
 }
 
+// functions that print and return parts of the list 
 
-function deleteGoal(name, goals[]){
-    for (let i=0; i < goal.length){
-        let temp = goals[i].
+function listGoalNames(goals){
+    const goalsNames = []
+    for(let i = 0; i < goals.length; i++){
+        goalsNames.push(returnGoalName(goals[i])); 
     }
-
-
+    return goalsNames; 
 }
+
+function listContentNames(goals){
+    const goalsContent = []; 
+    for(let i = 0; i < goals.length; i++){
+        goalsContent.push(returnGoalContent(goals[i]));
+    }
+    return goalsContent; 
+}
+
+
